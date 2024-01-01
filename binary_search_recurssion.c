dbe5787f858ca79e69ac13f19fffd544d0ca24b7
Gaurav <gauravdaultani8@gmail.com>
2024-01-01 23:30:42 +0530

    added binary search with recussion

---
+++ binary_search_recurssion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int target) {
+    if (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] > target) {
+            return binarySearch(arr, low, mid - 1, target);
+        }
+        else {
+            return binarySearch(arr, mid + 1, high, target);
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 12;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1) {
+        printf("Element not found\n");
+    }
+    else {
+        printf("Element found at index %d\n", result);
+    }
+
+    return 0;
+}
